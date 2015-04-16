3dc4583089b9dd6070085a8383e38bc31b113e09
Seth Wright <asethwright@gmail.com>
2015-04-16 14:04:23 -0400

    update viewmedica plugin to allow for dynamic user selection of viewmedica content. also fix a bug where the width parameter was not working properly.

--- swarm.php
+++ swarm.php
@@ -1,19 +1,19 @@
 <?php
 
 /*
-	Plugin Name: ViewMedica Embed
-	Plugin URI: http://swarminteractive.com/
-	Description: Allows easy embed of ViewMedica 6 into WordPress Posts and Pages.
-	Version: 0.2
-	Author: Seth Wright & Anthony Lobianco
-	Author URI: http://swarminteractive.com/
-	
-	Copyright 2011  ANTHONY_LOBIANCO SETH_WRIGHT  
-	
-	(email : anthony@swarminteractive.com, seth@swarminteractive.com)
+    Plugin Name: ViewMedica Embed
+    Plugin URI: http://swarminteractive.com/
+    Description: Allows easy embed of ViewMedica 7 into WordPress Posts and Pages. A full description, info and pull requests can be found at https://github.com/asethwright/viewmedica-wordpress
+    Version: 1.0
+    Author: Seth Wright & Anthony Lobianco
+    Author URI: http://swarminteractive.com/
+
+    Copyright 2011  ANTHONY_LOBIANCO SETH_WRIGHT
+
+    (email : anthony@swarminteractive.com, seth@swarminteractive.com)
 
     This program is free software; you can redistribute it and/or modify
-    it under the terms of the GNU General Public License, version 2, as 
+    it under the terms of the GNU General Public License, version 2, as
     published by the Free Software Foundation.
 
     This program is distributed in the hope that it will be useful,
@@ -28,157 +28,182 @@
 
 function swarm_install() {
 
-	global $wpdb;
-	
-	add_user_meta($user_id, 'swarm_ignore_notice', 'false', true);
-	
-	$table_name = $wpdb->prefix . 'viewmedica';
-	
-	$sql = "CREATE TABLE " . $table_name . " (
-				id mediumint(9) NOT NULL AUTO_INCREMENT,
-				vm_id mediumint(9),
-				vm_width mediumint(9),
-				vm_secure tinyint(1),
-				vm_brochures tinyint(1),
-				vm_fullscreen tinyint(1),
-				vm_disclaimer tinyint(1),
-				vm_language varchar(10),
-				UNIQUE KEY id (id)
-			)";
-			
-	require_once( ABSPATH . 'wp-admin/includes/upgrade.php');
-	dbDelta($sql);
-	
-	$rows_affected = $wpdb->insert( $table_name, array('id' => 1, 'vm_id' => '', 'vm_width' => 580, 'vm_secure' => 0, 'vm_fullscreen' => 1, 'vm_brochures' => 1, 'vm_disclaimer' => 1, 'vm_language' => 'en' ) );
-	
-	add_option('swarm_db_version', '1.0');
-	
+    global $wpdb;
+
+    add_user_meta($user_id, 'swarm_ignore_notice', 'false', true);
+
+    $table_name = $wpdb->prefix . 'viewmedica';
+
+    $sql = "CREATE TABLE " . $table_name . " (
+                id mediumint(9) NOT NULL AUTO_INCREMENT,
+                vm_id mediumint(9),
+                vm_width mediumint(9),
+                vm_secure tinyint(1),
+                vm_brochures tinyint(1),
+                vm_fullscreen tinyint(1),
+                vm_disclaimer tinyint(1),
+                vm_language varchar(10),
+                UNIQUE KEY id (id)
+            )";
+
+    require_once( ABSPATH . 'wp-admin/includes/upgrade.php');
+    dbDelta($sql);
+
+    $rows_affected = $wpdb->insert( $table_name, array('id' => 1, 'vm_id' => '', 'vm_width' => 580, 'vm_secure' => 0, 'vm_fullscreen' => 1, 'vm_brochures' => 1, 'vm_disclaimer' => 1, 'vm_language' => 'en' ) );
+
+    add_option('swarm_db_version', '1.0');
+
 }
 
-function swarm_settings() {  
-	
-	include_once('swarm-admin.php');
-	
+function swarm_settings() {
+
+    include_once('swarm-admin.php');
+
 }
 
 function swarm_admin_actions() {
 
-	add_options_page("Swarm Interactive", "Swarm Interactive", 10, "Swarm-Interactive-Admin", "swarm_settings");  
-	
-} 
+    add_options_page("ViewMedica", "ViewMedica", 10, "Viewmedica-Admin", "swarm_settings");
+
+}
 
 function swarm_viewmedica_display($atts = null, $content = null) {
-	
-	global $wpdb;
-	$table_name = $wpdb->prefix . 'viewmedica';
-	
-	$sql = "SELECT * FROM " . $table_name . "
-			WHERE id = 1";
-	$result = $wpdb->get_results($sql, 'OBJECT');
-	$result = $result[0];
-	$client = $result->vm_id;
-	$width = $result->vm_width;
-	$secure = $result->vm_secure;
-	$brochures = $result->vm_brochures;
-	$fullscreen = $result->vm_fullscreen;
-	$disclaimer = $result->vm_disclaimer;
-	$language = $result->vm_language;
-		
-	$param_string = '';
-	
-	if( $width != 580 ) {
-		$param_string .= 'width="'.$width.'"; ';
-	}
-	
-	if( $secure == 1 ) {
-		$param_string .= 'secure="true"; ';
-	}
-	
-	if( $brochures == 0 ) {
-		$param_string .= 'brochures="false"; ';
-	}
-	
-	if( $fullscreen == 0 ) {
-		$param_string .= 'fullscreen="false"; ';
-	}
-	
-	if( $disclaimer == 0 ) {
-		$param_string .= 'disclaimer="false"; ';
-	}
-	
-	if( $language != 'en' ) {
-		$param_string .= 'lang="'. $lang . '"; ';
-	}
-		
-	if( $atts != null ) {
-		$a = shortcode_atts( array( 'menuaccess' => '', 'openthis' => '' ), $atts );
-		$openthis = $a['openthis'];
-		$menuaccess = $a['menuaccess'];
-		
-		if( $openthis != '' ) {
-			$viewmedica_div = "<div id='" . $openthis . "'></div>";
-			$openthis_string = 'openthis="' . $openthis . '"; ';
-		} else {
-			$viewmedica_div = "<div id='vm'></div>";
-			$openthis_string = ' ';
-		}
-		
-		if( $menuaccess != '' ) {
-			$menuaccess_string = 'menuaccess="false"; ';
-		} else {
-			$menuaccess_string = '';
-		}
-		
-		$viewmedica_div .= "<script type='text/javascript'>client=\"" . $client . "\"; " . $param_string . $openthis_string . $menuaccess_string . "vm_open();</script>\n<!-- ViewMedica Embed End -->";
 
-	} else {
-		
-		$viewmedica_div = "<div id='vm'></div>";
-		
-		$viewmedica_div .= "<script type='text/javascript'>client=\"" . $client . "\"; vm_open();</script>\n<!-- ViewMedica Embed End -->";
-	
-	}
-	
-	return $viewmedica_div;
-	
+    global $wpdb;
+    $table_name = $wpdb->prefix . 'viewmedica';
+
+    $sql = "SELECT * FROM " . $table_name . "
+            WHERE id = 1";
+    $result = $wpdb->get_results($sql, 'OBJECT');
+    $result = $result[0];
+    $client = $result->vm_id;
+    $global_width = $result->vm_width;
+    $secure = $result->vm_secure;
+    $brochures = $result->vm_brochures;
+    $fullscreen = $result->vm_fullscreen;
+    $disclaimer = $result->vm_disclaimer;
+    $language = $result->vm_language;
+
+    $param_string = '';
+
+    if( $secure == 1 ) {
+        $param_string .= 'secure="true"; ';
+    }
+
+    if( $brochures == 0 ) {
+        $param_string .= 'brochures="false"; ';
+    }
+
+    if( $fullscreen == 0 ) {
+        $param_string .= 'fullscreen="false"; ';
+    }
+
+    if( $disclaimer == 0 ) {
+        $param_string .= 'disclaimer="false"; ';
+    }
+
+    if( $language != 'en' ) {
+        $param_string .= 'lang="'. $lang . '"; ';
+    }
+
+    if( $atts != null ) {
+        $a = shortcode_atts( array( 'menuaccess' => '', 'openthis' => '', 'width' => '' ), $atts );
+        $openthis = $a['openthis'];
+        $menuaccess = $a['menuaccess'];
+        $width = $a['width'];
+
+        if( $openthis != '' ) {
+            $viewmedica_div = "<div id='" . $openthis . "'></div>";
+            $openthis_string = 'openthis="' . $openthis . '"; ';
+        } else {
+            $viewmedica_div = "<div id='vm'></div>";
+            $openthis_string = ' ';
+        }
+
+        if( $menuaccess != '' ) {
+            $menuaccess_string = 'menuaccess="false"; ';
+        } else {
+            $menuaccess_string = '';
+        }
+
+        if( $width != '' ) {
+            $width_string = 'width=' . $width . '; ';
+        } else {
+            $width_string = 'width=' . $global_width . '; ';
+        }
+
+        $viewmedica_div .= "<script type='text/javascript'>client=\"" . $client . "\"; " . $width_string .  $param_string . $openthis_string . $menuaccess_string . "vm_open();</script>\n<!-- ViewMedica Embed End -->";
+
+    } else {
+
+        $viewmedica_div = "<div id='vm'></div>";
+
+        $viewmedica_div .= "<script type='text/javascript'>client=\"" . $client . "\"; width=\"" . $global_width . "\"; vm_open();</script>\n<!-- ViewMedica Embed End -->";
+
+    }
+
+    return $viewmedica_div;
+
 }
 
 function swarm_header() {
-	
-	wp_register_script('viewmedicascript', 'https://swarminteractive.com/js/vm.js', array(),
+
+    wp_register_script('viewmedicascript', 'https://swarminteractive.com/js/vm.js', array(),
        '1.0', false );
     wp_enqueue_script( 'viewmedicascript' );
-	
+
 }
 
 function add_viewmedica_button() {
-   // Don't bother doing this stuff if the current user lacks permissions
-   if ( ! current_user_can('edit_posts') && ! current_user_can('edit_pages') )
-     return;
- 
-   // Add only in Rich Editor mode
-   if ( get_user_option('rich_editing') == 'true') {
-     add_filter("mce_external_plugins", "add_viewmedica_tinymce_plugin");
-     add_filter('mce_buttons', 'register_viewmedica_button');
-   }
+    // Don't bother doing this stuff if the current user lacks permissions
+    if ( ! current_user_can('edit_posts') && ! current_user_can('edit_pages') )
+        return;
+
+    // Add only in Rich Editor mode
+    if ( get_user_option('rich_editing') == 'true') {
+        add_filter("tiny_mce_before_init", "add_viewmedica_clientid");
+        add_filter("mce_external_plugins", "add_viewmedica_tinymce_plugin");
+        add_filter('mce_buttons', 'register_viewmedica_button');
+    }
 }
- 
+
+function add_viewmedica_clientid($settings)
+{
+    global $wpdb;
+    $table_name = $wpdb->prefix . 'viewmedica';
+
+    $sql = "SELECT * FROM " . $table_name . "
+            WHERE id = 1";
+    $result = $wpdb->get_results($sql, 'OBJECT');
+    $result = $result[0];
+
+    $settings['vm_client'] = $result->vm_id;
+    $settings['vm_width'] = $result->vm_width;
+    $settings['vm_secure'] = $result->vm_secure;
+    $settings['vm_brochures'] = $result->vm_brochures;
+    $settings['vm_fullscreen'] = $result->vm_fullscreen;
+    $settings['vm_disclaimer'] = $result->vm_disclaimer;
+    $settings['vm_language'] = $result->vm_language;
+
+    return $settings;
+}
+
 function register_viewmedica_button($buttons) {
-   array_push($buttons, "|", "viewmedica");
-   return $buttons;
+    array_push($buttons, "|", "viewmedica");
+    return $buttons;
 }
- 
+
 // Load the TinyMCE plugin : editor_plugin.js (wp2.5)
 function add_viewmedica_tinymce_plugin($plugin_array) {
-   $plugin_array['viewmedica'] = plugins_url() .'/viewmedica/swarm_plugin.js';
-   return $plugin_array;
+    $plugin_array['viewmedica'] = plugins_url() .'/viewmedica/swarm_plugin.js';
+    return $plugin_array;
 }
- 
+
 function vm_refresh_mce($ver) {
-  $ver += 3;
-  return $ver;
+    $ver += 3;
+    return $ver;
 }
- 
+
 function swarm_admin_notice() {
     global $current_user ;
         $user_id = $current_user->ID;
@@ -200,14 +225,14 @@ function swarm_nag_ignore( $updated = false ) {
 }
 
 function swarm_uninstall() {
-	
-	global $wpdb;
-	
-	$table_name = $wpdb->prefix . 'viewmedica';
-	
-	$sql = 'DROP TABLE ' . $table_name;
-	
-	$wpdb->query($sql);
+
+    global $wpdb;
+
+    $table_name = $wpdb->prefix . 'viewmedica';
+
+    $sql = 'DROP TABLE ' . $table_name;
+
+    $wpdb->query($sql);
 
 }
 
--- swarm_plugin.js
+++ swarm_plugin.js
@@ -1,93 +1,156 @@
+var vm_client;
+var vm_width;
+var vm_secure;
+
 (function() {
-	tinymce.create('tinymce.plugins.ViewMedica', {
-		
-		init : function(ed, url) {
-			
-			ed.addButton('viewmedica', {
-				title : 'viewmedica.embed',
-				image : url+'/viewmedica.png',
-				onclick : function() {
-					var width = jQuery(window).width(), H = jQuery(window).height(), W = ( 720 < width ) ? 720 : width;
-					W = W - 80;
-					H = H - 84;
-					tb_show('Viewmedica Options', '#TB_inline?width='+W+'&height='+H+'&inlineId=viewmedica-form');
-					/*var clientid = prompt("Client ID", "Enter the id of your swarm account");
-					if (clientid != null && clientid != 'undefined')
-						ed.execCommand('mceInsertContent', false, '[viewmedica client="'+clientid+'"]');*/
-				}
-			});
-		},
-		
-		createControl : function(n, cm) {
-			return null;
-		},
-		
-		getInfo : function() {
-			return {
-				longname : "ViewMedica Embed",
-				author : 'Seth Wright & Anthony Lobianco',
-				authorurl : 'http://swarminteractive.com/',
-				infourl : 'http://swarminteractive.com/',
-				version : "1.0"
-			};
-		}
-	});
-	
-	tinymce.PluginManager.add('viewmedica', tinymce.plugins.ViewMedica);
-	
+    tinymce.create('tinymce.plugins.ViewMedica', {
+
+        init : function(ed, url) {
+            vm_client = ed.settings.vm_client;
+            vm_width = ed.settings.vm_width;
+            vm_secure = ed.settings.vm_secure;
+            vm_plugin_init();
+
+            ed.addButton('viewmedica', {
+                title : 'viewmedica.embed',
+                image : url+'/viewmedica.png',
+                onclick : function() {
+                    var width = jQuery(window).width(), H = jQuery(window).height(), W = ( 720 < width ) ? 720 : width;
+                    W = W - 80;
+                    H = H - 84;
+                    tb_show('Viewmedica Options', '#TB_inline?width='+W+'&height='+H+'&inlineId=viewmedica-form');
+                }
+            });
+        },
+
+        createControl : function(n, cm) {
+            return null;
+        },
+
+        getInfo : function() {
+            return {
+                longname : "ViewMedica Embed",
+                author : 'Seth Wright & Anthony Lobianco',
+                authorurl : 'http://swarminteractive.com/',
+                infourl : 'http://swarminteractive.com/',
+                version : "1.0"
+            };
+        }
+    });
+
+    tinymce.PluginManager.add('viewmedica', tinymce.plugins.ViewMedica);
+
 })();
 
-jQuery( function() {
+function vm_plugin_init() {
 
-	var form = jQuery('<div id="viewmedica-form"><table id="viewmedica-table" class="form-table">\
-			<tr>\
-				<th><label for="viewmedica-openthis">Openthis Code</label></th>\
-				<td><input type="text" id="viewmedica_openthis" name="openthis" value="" /><br />\
-				<small>Input desired opening screen, or leave blank for main</small></td>\
-			</tr>\
-			<tr>\
-				<th><label for="viewmedica-menuaccess">Turn Off Menu Access</label></th>\
-				<td><input type="checkbox" id="viewmedica_menuaccess" name="menuaccess" value="false" /><br />\
-				<small>Do not let users navigate through ViewMedica</small></td>\
-			</tr>\
-			<tr>\
-				<th>Get a List of Your Animation Codes</th>\
-				<td><a href="http://swarminteractive.com/vm/login" target="_blank">Login to your Swarm Interactive account</a> and click Installation Support.</td>\
-			</tr>\
-		</table>\
-		<p class="submit">\
-			<input type="button" id="viewmedica-submit" class="button-primary" value="Insert Viewmedica Shortcode" name="submit" />\
-		</p>\
-		</div>');
-		
-	var table = form.find('table');
-	form.appendTo('body').hide();
-		
-	form.find('#viewmedica-submit').click(function(){
-			var options = { 
-				'openthis': '',
-			};
-			var shortcode = '[viewmedica';
-			
-			for( var index in options) {
-				var value = table.find('#viewmedica_' + index).val();
-				
-				// attaches the attribute to the shortcode only if it's different from the default value
-				if ( value !== options[index] && value != '' )
-					shortcode += ' ' + index + '="' + value + '"';
-			}
-						
-			if( table.find('#viewmedica_menuaccess').attr('checked') == 'checked' ) {
-				shortcode += ' menuaccess="false"';
-			}
-			
-			shortcode += ']';
-			
-			// inserts the shortcode into the active editor
-			tinyMCE.activeEditor.execCommand('mceInsertContent', 0, shortcode);
-			
-			// closes Thickbox
-			tb_remove();
-	});
+    jQuery(function() {
 
-});
+        var form = jQuery('<div id="viewmedica-form"><table id="viewmedica-table" class="form-table">\
+                <tr>\
+                    <th><label for="viewmedica-menuaccess">Turn Off Menu Access</label></th>\
+                    <td><input type="checkbox" id="viewmedica_menuaccess" name="menuaccess" value="false" /><br />\
+                    <small>Do not let users navigate through ViewMedica</small></td>\
+                </tr>\
+                <tr>\
+                    <th><label for="viewmedica-width">Player Width</label></th>\
+                    <td><input type="text" id="viewmedica_width" name="width" value="" /><br />\
+                    <small>Set a maximum width for the ViewMedica content. Leave blank for default set on options page.</small></td>\
+                </tr>\
+                <tr>\
+                    <th><label for="viewmedica-openthis">Open Location</label></th>\
+                    <td id="open-selector">Loading...</td>\
+                </tr>\
+                <tr>\
+                    <th><label for="viewmedica-openthis">Openthis Code</label></th>\
+                    <td><input type="text" id="viewmedica_openthis" name="openthis" value="" /><br />\
+                    <small>Input desired opening screen, or choose from the list above. Leave blank for main.</small></td>\
+                </tr>\
+            </table>\
+            <p class="submit">\
+                <input type="button" id="viewmedica-submit" class="button-primary" value="Insert Viewmedica Shortcode" name="submit" />\
+            </p>\
+        </div>');
+
+        jQuery.ajax({
+            dataType: "json",
+            url: 'http://swarminteractive.com/vm/index/client_json/' + vm_client,
+            success: function(data) {
+                build(data, form);
+            }
+        });
+
+        var table = form.find('table');
+        form.appendTo('body').hide();
+
+        form.find('#viewmedica-submit').click(function(){
+                var options = {
+                    'openthis': '',
+                    'width': '',
+                };
+                var shortcode = '[viewmedica';
+
+                for( var index in options) {
+                    var value = table.find('#viewmedica_' + index).val();
+
+                    // attaches the attribute to the shortcode only if it's different from the default value
+                    if ( value !== options[index] && value != '' )
+                        shortcode += ' ' + index + '="' + value + '"';
+                }
+
+                if( table.find('#viewmedica_menuaccess').attr('checked') == 'checked' ) {
+                    shortcode += ' menuaccess="false"';
+                }
+
+                shortcode += ']';
+
+                // inserts the shortcode into the active editor
+                tinyMCE.activeEditor.execCommand('mceInsertContent', 0, shortcode);
+
+                // closes Thickbox
+                tb_remove();
+        });
+
+    });
+
+}
+
+function build(profile, form)
+{
+    var html = '<select id="vm-open" style="width: 100%"><option value="">Main Embed</option>\
+                    ';
+
+    for (var lib_key in profile.libraries) {
+
+        var l = profile.libraries[lib_key];
+        html += '<option value="' + lib_key + '">' + l.label + '</option>';
+
+        for (var col_key in profile.libraries[lib_key].collections) {
+
+            var c = profile.libraries[lib_key].collections[col_key];
+            html += '<option value="' + col_key + '">-- ' + c.label + '</option>';
+
+            for (var grp_key in profile.libraries[lib_key].collections[col_key].groups) {
+
+                var g = profile.libraries[lib_key].collections[col_key].groups[grp_key];
+                html += '<option value="' + grp_key + '">---- ' + g.label + '</option>';
+
+                for (var item_key in profile.libraries[lib_key].collections[col_key].groups[grp_key].items) {
+                    var r = profile.libraries[lib_key].collections[col_key].groups[grp_key].items[item_key];
+                    var label = r.label.replace('|', "");
+                    html += '<option value="' + item_key + '">------ ' + label + '</option>';
+                }
+
+            }
+        }
+    }
+
+    html+= '</select>';
+
+    form.find('#open-selector').html(html);
+
+    form.find('#vm-open').change(function() {
+        jQuery('#viewmedica_openthis').val( jQuery(this).val() );
+    });
+
+}
